7258afcc3cea55a73f2af8119ad7e42ec8cd8e3a
Wallteg <carl.wallteg@yh.nackademin.se>
2018-03-23 15:34:54 +0100

    connection to SQL myReader Variables

--- Grupparbete_Insurance/Form1.cs
+++ Grupparbete_Insurance/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace Grupparbete_Insurance
 {
@@ -19,7 +20,80 @@ namespace Grupparbete_Insurance
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            //Connection med Sql server, databas Insurance.
+            SqlConnection conn = new SqlConnection();
+            
+            conn.ConnectionString = "Data Source = DESKTOP - 0KMEDJA\\SQL2017; Initial Catalog = Insurance; Integrated Security = True";
 
+            try
+            {
+                conn.Open(); // öppna kopplingen
+                
+                //Vi vill komma åt tabellen Car_Insurance som innehåller vår data.
+                SqlCommand myQuery = new SqlCommand("SELECT * FROM Car_Insurance;", connection: conn);
+
+                SqlDataReader myReader = myQuery.ExecuteReader();
+
+                //Vi inleder med att deklarera våra olika variabler som vi har att tillgå från SQL server
+                int id;
+                int age;
+                string job;
+                string marital;
+                string education;
+                bool credit;
+                int balance;
+                bool homeInsurance;
+                bool carLoan;
+                string communication;
+                int lastContactDay;
+                string lastContactMonth;
+                int noOfContacts;
+                int daysPassed;
+                int prevAttemts;
+                string outcome;
+                TimeSpan callStart;
+                TimeSpan callEnd;
+                bool carInsurance;
+
+                while (myReader.Read())
+                {
+                    id = (int)myReader["id"];
+                    age = (int)myReader["age"];
+                    job = myReader["job"].ToString();
+                    marital = myReader["martial"].ToString();
+                    education = myReader["education"].ToString();
+                    if ((int)myReader["default"] == 1)
+                    {
+                        credit = true;
+                    }
+                    else
+                    {
+                        credit = false;
+                    }
+                    balance = (int)myReader["balance"];
+                    if ((int)myReader["HHInsurance"] == 1)
+                    {
+                        homeInsurance = true;
+                    }
+                    else
+                    {
+                        homeInsurance = false;
+                    }
+
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            finally
+            {
+                conn.Close();
+            }
         }
+
     }
+    
 }
