7d6c5f6e787de5af4b7543966d5f7e89ddca92f6
Wallteg <carl.wallteg@yh.nackademin.se>
2018-03-28 09:59:30 +0200

    Sätt att räkna ut procent

--- Grupparbete_Insurance/Form1.cs
+++ Grupparbete_Insurance/Form1.cs
@@ -140,12 +140,26 @@ var hejMomina = from Customer in customerList
 
 
 
-            for (int i = 17; i < 95; i++) // Plockar ut ålder, grupperar och räknar antalet.
+            //   chart1.DataManipulator.GroupByAxisLabel("Antal", "age");
+            for (int i = 17; i <= 90; i++) // Plockar ut ålder, grupperar och räknar antalet.
             {
-                var q = customerList.Where(y => y.age == i && y.carInsurance == 1);
-                chart1.Series["Series1"].Points.AddXY(i, q.Count());
+                var ins = customerList.Where(y => y.age == i && y.carInsurance == 1);
+                //var byage = customerList.GroupBy(y => y.age);
+                chart1.Series["Bilförsäkring"].Points.AddXY(i, ins.Count());
+
+                var noIns = customerList.Where(y => y.age == i && y.carInsurance == 0);
+                chart1.Series["Utan försäkring"].Points.AddXY(i, noIns.Count());
             }
 
+
+
+            /*   foreach (Customer c in customerList)
+               {
+                   var q = customerList.Where(y => y.carInsurance == 1);
+                   chart1.Series["Series1"].Points.AddXY(c.education, q.Count());
+
+               };
+            */
 
             /*
             foreach (Customer c in customerList.Where(y => y.carInsurance == 1))
@@ -153,13 +167,57 @@ var hejMomina = from Customer in customerList
                 chart1.Series["Series1"].Points.AddXY(c.age,c.);
             } 
             */
-            chart1.Series["Series1"].ChartType = SeriesChartType.Column;
-            chart1.Titles.Add("Age");
-            chart1.ChartAreas[0].AxisY.Title = "Balance";
+            chart1.Series["Utan försäkring"].ChartType = SeriesChartType.Line;
+            chart1.Series["Bilförsäkring"].ChartType = SeriesChartType.Line;
+            chart1.Titles.Add("Spridning av kundersålder och koppling till om dem har eller inte har försäkring");
+            chart1.ChartAreas[0].AxisY.Title = "Antal";
             chart1.ChartAreas[0].AxisX.Title = "Ålder";
+            chart1.Series["Utan försäkring"].BorderWidth = 5;
+            chart1.Series["Bilförsäkring"].BorderWidth = 5;
 
 
-            //   chart1.DataManipulator.GroupByAxisLabel("Antal", "age");
+
+
+            /*
+                        foreach (var Customer in nameGroup)
+                        {
+                            chart1.Series["Series1"].Points.AddXY(Customer.age, Customer.balance);
+                        }
+
+            */
+            var groups = customerList.Where(c => c.carInsurance == 1).GroupBy(c => c.age).OrderBy(g => g.Key).ToList();
+
+            int totalCount = groups.Sum(g => g.Count());
+
+            int countUppTill25 = groups.Where(g => g.Key <= 25).Sum(g => g.Count());
+            int count26Till32 = groups.Where(g => g.Key >= 26 && g.Key <= 32).Sum(g => g.Count());
+            int count33Till40 = groups.Where(g => g.Key >= 33 && g.Key <= 40).Sum(g => g.Count());
+            int count41Till50 = groups.Where(g => g.Key >= 41 && g.Key <= 50).Sum(g => g.Count());
+            int count51Till65 = groups.Where(g => g.Key >= 51 && g.Key <= 65).Sum(g => g.Count());
+            int count66OchAldre = groups.Where(g => g.Key >= 66).Sum(g => g.Count());
+
+            Series serice1 = chart2.Series["Series1"]; // Hämtar serien från diagramet
+            serice1.Points.AddXY("0 - 25", ((decimal)countUppTill25 / totalCount) * 100);
+            serice1.Points.AddXY("26 - 32", ((decimal)count26Till32 / totalCount) * 100);
+            serice1.Points.AddXY("33 - 40", ((decimal)count33Till40 / totalCount) * 100);
+            serice1.Points.AddXY("41 - 50", ((decimal)count41Till50 / totalCount) * 100);
+            serice1.Points.AddXY("51 - 65", ((decimal)count51Till65 / totalCount) * 100);
+            serice1.Points.AddXY("66+", ((decimal)count66OchAldre / totalCount) * 100);
+
+            chart2.Series["Series1"].ChartType = SeriesChartType.RangeColumn;
+            chart2.Titles.Add("Procentuell");
+            chart2.ChartAreas[0].AxisY.Title = "Antal i procent";
+            chart2.ChartAreas[0].AxisX.Title = "Ålder";
+            chart2.Series["Series1"].BorderWidth = 5;
+
+
+            //Series serice1 = chart1.Series["Series1"]; // Hämtar serien från diagramet
+            //serice1.Points.AddXY("0 - 25", countUppTill25);
+            //serice1.Points.AddXY("26 - 32", count26Till32);
+            //serice1.Points.AddXY("33 - 40", count33Till40);
+            //serice1.Points.AddXY("41 - 50", count41Till50);
+            //serice1.Points.AddXY("51 - 65", count51Till65);
+            //serice1.Points.AddXY("66+", count66OchAldre);
         }
     }
 }
