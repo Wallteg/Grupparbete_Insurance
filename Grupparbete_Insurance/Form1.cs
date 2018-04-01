dcee9039713f422e6cfa9a147f5fba5b4c32ce68
Wallteg <carl.wallteg@yh.nackademin.se>
2018-04-01 20:38:37 +0200

    Added comments for the code

--- Grupparbete_Insurance/Form1.cs
+++ Grupparbete_Insurance/Form1.cs
@@ -24,8 +24,10 @@ namespace Grupparbete_Insurance
             //Connection med Sql server, databas Insurance.
             SqlConnection conn = new SqlConnection();
 
+            //Skapar en koppling till Databasen "Insurance" som finns på SQL server.
             conn.ConnectionString = "Data Source=DESKTOP-0KMEDJA\\SQL2017;Initial Catalog=Insurance;Integrated Security=True";
-
+            
+            //Skapar en kundlista där vi sedan kan spara alla kunduppgifter om våra kunder.
             List<Customer> customerList = new List<Customer>();
 
             try
@@ -45,10 +47,10 @@ namespace Grupparbete_Insurance
                 string job;
                 string marital;
                 string education;
-                int credit; // bool?    1 true 0 false
+                int credit; //    1 true 0 false
                 int balance;
-                int homeInsurance; // bool?     1 true 0 false
-                int carLoan;  // bool?      1 true 0 false
+                int homeInsurance; //      1 true 0 false
+                int carLoan;  //       1 true 0 false
                 string communication;
                 int lastContactDay;
                 string lastContactMonth;
@@ -58,12 +60,13 @@ namespace Grupparbete_Insurance
                 string outcome;
                 TimeSpan callStart;
                 TimeSpan callEnd;
-                int carInsurance; // bool?   1 true 0 false
+                int carInsurance; //  1 true 0 false
 
 
                 while (myReader.Read())
                 {
 
+                    //inhämtning av värden från SQL server till våra olika variabler. 
                     id = (int)myReader["id"];
                     age = (int)myReader["age"];
                     job = myReader["job"].ToString();
@@ -84,12 +87,12 @@ namespace Grupparbete_Insurance
                     callEnd = (TimeSpan)myReader["callend"];
                     carInsurance = (int)myReader["carinsurance"];
 
-
+                    //Koppling till vår klass "Customer", varje kund blir ett objekt med dessa olika egenskaperna.
                     Customer customer = new Customer(id, age, job, marital, education, credit, balance,
                         homeInsurance, carLoan, communication, lastContactDay, lastContactMonth, noOfContacts,
                         daysPassed, prevAttemts, outcome, callStart, callEnd, carInsurance);
 
-
+                    // Lägg till det i customerList som skapades inledningsvis på denna sida.
                     customerList.Add(customer);
                 }
 
@@ -103,7 +106,8 @@ namespace Grupparbete_Insurance
                 conn.Close();
             }
 
-
+            //För vår visualisering vill vi kunna särskilja personer som har försäkring och dem som inte har försäkring
+            // Vi har olika intressepunkter och olika områden vi vill undersöka, såsom, Ålder, Utbildning, Civilstatus, Jobb, osv.
             var groups = customerList.Where(c => c.carInsurance == 1).GroupBy(c => c.age).OrderBy(g => g.Key).ToList();
             var groups1 = customerList.Where(c => c.carInsurance == 0).GroupBy(cn => cn.age).OrderBy(gn => gn.Key).ToList();
             var edu = customerList.Where(c => c.carInsurance == 1).GroupBy(c => c.education).OrderBy(ed => ed.Key).ToList();
@@ -115,9 +119,11 @@ namespace Grupparbete_Insurance
             var home = customerList.Where(c => c.carInsurance == 1).GroupBy(c => c.homeInsurance).OrderBy(h => h.Key).ToList();
             var home1 = customerList.Where(c => c.carInsurance == 0).GroupBy(c => c.homeInsurance).OrderBy(h => h.Key).ToList();
 
+
             int counter = customerList.Count(); // Beräknar antalet rader för att kunna få ut procent
 
-
+            //vi vill kunna se åldergrupper på våra kunder för att kunna veta vart vi ska lägga största fokus,
+            // För att kunna göra det använder vi denna för att kunna lägga ihop ett samlat resultat för dem som är inom samma åldersspann.
             int countUppTill25 = groups.Where(g => g.Key <= 25).Sum(g => g.Count());
             int count26Till32 = groups.Where(g => g.Key >= 26 && g.Key <= 32).Sum(g => g.Count());
             int count33Till40 = groups.Where(g => g.Key >= 33 && g.Key <= 40).Sum(g => g.Count());
@@ -125,6 +131,7 @@ namespace Grupparbete_Insurance
             int count51Till65 = groups.Where(g => g.Key >= 51 && g.Key <= 65).Sum(g => g.Count());
             int count66OchAldre = groups.Where(g => g.Key >= 66).Sum(g => g.Count());
 
+            //Denna gör samma sak, men räknar ut dem som SAKNAR försäkring, den ovan får ut resultatet för personer som är försäkrade.
             int countUppTill25n = groups1.Where(gn => gn.Key <= 25).Sum(gn => gn.Count());
             int count26Till32n = groups1.Where(gn => gn.Key >= 26 && gn.Key <= 32).Sum(gn => gn.Count());
             int count33Till40n = groups1.Where(gn => gn.Key >= 33 && gn.Key <= 40).Sum(gn => gn.Count());
@@ -132,7 +139,7 @@ namespace Grupparbete_Insurance
             int count51Till65n = groups1.Where(gn => gn.Key >= 51 && gn.Key <= 65).Sum(gn => gn.Count());
             int count66OchAldren = groups1.Where(gn => gn.Key >= 66).Sum(gn => gn.Count());
 
-
+            //Samma princip fast här tar vi reda på antalet personer som har en viss utbildningsgrad.
             int primary = edu.Where(ed => ed.Key == "primary").Sum(ed => ed.Count());
             int secondary = edu.Where(ed => ed.Key == "secondary").Sum(ed => ed.Count());
             int tertiary = edu.Where(ed => ed.Key == "tertiary").Sum(ed => ed.Count());
@@ -143,7 +150,7 @@ namespace Grupparbete_Insurance
             int tertiaryn = edu1.Where(ed => ed.Key == "tertiary").Sum(ed => ed.Count());
             int NAn = edu1.Where(ed => ed.Key == "NA").Sum(ed => ed.Count());
 
-
+            //Civilstatus
             int married = Marital.Where(m => m.Key == "married").Sum(m => m.Count());
             int single = Marital.Where(m => m.Key == "single").Sum(m => m.Count());
             int divorced = Marital.Where(m => m.Key == "divorced").Sum(m => m.Count());
@@ -152,7 +159,7 @@ namespace Grupparbete_Insurance
             int singlen = Marital1.Where(m => m.Key == "single").Sum(m => m.Count());
             int divorcedn = Marital1.Where(m => m.Key == "divorced").Sum(m => m.Count());
 
-
+            //Yrke
             int management = jobs.Where(j => j.Key == "management").Sum(j => j.Count());
             int technician = jobs.Where(j => j.Key == "technician").Sum(j => j.Count());
             int bluecollar = jobs.Where(j => j.Key == "blue-collar").Sum(j => j.Count());
@@ -177,12 +184,17 @@ namespace Grupparbete_Insurance
             int entrepreneurn = jobs1.Where(j => j.Key == "entrepreneur").Sum(j => j.Count());
             int housemaidn = jobs.Where(j => j.Key == "housemaid").Sum(j => j.Count());
 
+            // Vi ville bland annat kunna se om det var vanligt att samla sina försäkringar på samma ställe inom banken
+            // Denna del hjälper oss att få fram antalet kunder som har båda försäkringarna, endast bil, endast hem eller saknar försäkring inom banken
             int homecar = home.Where(h => h.Key == 1).Sum(h => h.Count());
             int noHomeCar = home.Where(h => h.Key == 0).Sum(h => h.Count());
 
             int homeNoCar = home1.Where(h => h.Key == 1).Sum(h => h.Count());
             int noHomeNoCar = home1.Where(h => h.Key == 0).Sum(h => h.Count());
 
+
+            //Vi gör en serie som vi kallar för ageIns (ålder Försäkrade), den ska läggas till i seriens Age Insured
+            //raderna under så lägger vi till ett X-värde bestående av åldersspannet, därefter lägger vi till ett värde i Procent.
             Series ageIns = chart1.Series["Age Insured"]; // Hämtar serien från diagramet
             ageIns.Points.AddXY("0 - 25", ((decimal)countUppTill25 / counter) * 100);
             ageIns.Points.AddXY("26 - 32", ((decimal)count26Till32 / counter) * 100);
@@ -190,9 +202,9 @@ namespace Grupparbete_Insurance
             ageIns.Points.AddXY("41 - 50", ((decimal)count41Till50 / counter) * 100);
             ageIns.Points.AddXY("51 - 65", ((decimal)count51Till65 / counter) * 100);
             ageIns.Points.AddXY("66+", ((decimal)count66OchAldre / counter) * 100);
-            ageIns.Enabled = false;
+            ageIns.Enabled = false; // Vi gör grafen ej synlig som default, för att med hjälp av knappar senare kunna visa önskad chart.
 
-
+            // Serie för ålderspann fast för kunder som saknar försäkring.
             Series ageNoIns = chart1.Series["Age Uninsured"]; // Hämtar serien från diagramet
             ageNoIns.Points.AddXY("0 - 25", ((decimal)countUppTill25n / counter) * 100);
             ageNoIns.Points.AddXY("26 - 32", ((decimal)count26Till32n / counter) * 100);
@@ -200,9 +212,9 @@ namespace Grupparbete_Insurance
             ageNoIns.Points.AddXY("41 - 50", ((decimal)count41Till50n / counter) * 100);
             ageNoIns.Points.AddXY("51 - 65", ((decimal)count51Till65n / counter) * 100);
             ageNoIns.Points.AddXY("66+", ((decimal)count66OchAldren / counter) * 100);
-            ageNoIns.Enabled = false;
+            ageNoIns.Enabled = false; // Gör grafen ej synlig vid uppstart.
 
-
+            //Skapar en serie gör utbildning
             Series educationIns = chart1.Series["Education Insured"]; // Hämtar serien från diagramet
             educationIns.Enabled = false;
 
@@ -227,6 +239,7 @@ namespace Grupparbete_Insurance
             maritalIns.Points.AddXY("married", ((decimal)married / counter) * 100);
             maritalIns.Points.AddXY("single", ((decimal)single / counter) * 100);
             maritalIns.Points.AddXY("divorced", ((decimal)divorced / counter) * 100);
+            
             //Dom som inte har försäkring
             Series maritalNoIns = chart1.Series["Marital Status Uninsured"]; // Hämtar serien från diagramet
             maritalNoIns.Enabled = false;
@@ -277,13 +290,20 @@ namespace Grupparbete_Insurance
         private void button1_Click_1(object sender, EventArgs e)
         {
             {
+
+                //Knapp med click funktion, den första är för att få upp åldersstatistiken på vår chart.
                 Series ageIns = chart1.Series["Age Insured"];
                 Series ageNoIns = chart1.Series["Age Uninsured"];
 
+
+                //Då vi tidigare i koden satt dessa conditions till false så kommer dem till en början alltid inledas i ett läge där
+                //Grafen är tom. genom att klicka på knapparna kommer man sedan få fram önskad information.
                 if (ageIns.Enabled == false && ageNoIns.Enabled == false)
                 {
-                    ageIns.Enabled = true;
+                    //Inget visas
+                    ageIns.Enabled = true; // ändrar till true så att nästa gång man trycker på knappen ska den komma in i nästa steg.
 
+                    //Inställningar för hur grafen ska se ut, såsom chart type, titel, namn på x/y axel, tjocklek på visaren och höjd på y axeln.
                     chart1.Series["Age Insured"].ChartType = SeriesChartType.Line;
                     chart1.Series["Age Uninsured"].ChartType = SeriesChartType.Line;
                     chart1.Titles.Add("Spridning av kundersålder och koppling till om dem har eller inte har försäkring");
@@ -297,21 +317,26 @@ namespace Grupparbete_Insurance
                 }
                 else if (ageIns.Enabled == true && ageNoIns.Enabled == false)
                 {
+                    // Nu visas endast de som har försäkring i dem olika åldrarna.
                     ageIns.Enabled = false;
                     ageNoIns.Enabled = true;
 
                 }
                 else if (ageIns.Enabled == false && ageNoIns.Enabled == true)
                 {
+
+                    // Nu visas endast de som inte är försäkrade.
                     ageNoIns.Enabled = true;
                     ageIns.Enabled = true;
 
                 }
                 else if (ageIns.Enabled == true && ageNoIns.Enabled == true)
                 {
+                    // Nu vill vi kunna jämföra de försäkrade mot dem som saknar försäkring för att kunna hitta olika trender.
                     ageIns.Enabled = false;
                     ageNoIns.Enabled = false;
 
+                    // Vid sista steget tömmer den charten för att vi ska kunna visa en ny chart på ett nytt område.
                     chart1.Titles.Clear();
                 }
             }
@@ -320,13 +345,14 @@ namespace Grupparbete_Insurance
         private void button2_Click(object sender, EventArgs e)
         {
             {
-
+                // samma sak som förra knappen fast nytt område. (Utbildningsgrad).
                 Series educationIns = chart1.Series["Education Insured"];
                 Series educationNoIns = chart1.Series["Education Uninsured"];
 
 
                 if (educationIns.Enabled == false && educationNoIns.Enabled == false)
                 {
+                    //Börjar vara tom, sen får man ny visning för varje knapptryck
                     educationIns.Enabled = true;
 
                     chart1.Titles.Add("Education");
@@ -452,10 +478,11 @@ namespace Grupparbete_Insurance
                     chart1.Titles.Add("Insurance types");
                     chart1.ChartAreas[0].AxisY.Title = "Amount in Percentage";
                     chart1.ChartAreas[0].AxisX.Title = "Insurances";
-                    chart1.ChartAreas["ChartArea1"].AxisX.Interval = 1;
+                    chart1.ChartAreas["ChartArea1"].AxisX.Interval = 1; // Då vi hade många olika yrken behövde vi denna rad för att den 
+                    //Skulle skriva ut "yrekstiteln" vid varje x punkt.
 
-                    chart1.ChartAreas[0].AxisY.Maximum = 40;
-                    chart1.ChartAreas[0].AxisY.Minimum = 0;
+                    chart1.ChartAreas[0].AxisY.Maximum = 40; // Visa diagrammet upp till 40% på Y axeln.
+                    chart1.ChartAreas[0].AxisY.Minimum = 0; // Börja på 0.
                 }
                 else if (insurance.Enabled == true)
                 {
